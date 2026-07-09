b5e12dcb8c2300204c666f9a439b798d6c95957a
Daniel <danieljaimes.2001@gmail.com>
2026-07-09 18:11:37 -0500

    fix: configurar ruta GET /api/inventario para cargar datos

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const inventoryController = require('../controllers/inventoryController');
 
-// La ruta final será /api/inventario/actualizar
-router.put('/actualizar', inventoryController.actualizarInventario);
 
+// Aquí defines que el GET (leer inventario) y el PUT (actualizar) pasen por ahí
+router.get('/:machine_id', inventoryController.obtenerInventario); 
+router.put('/actualizar', inventoryController.actualizarInventario);
 module.exports = router;
