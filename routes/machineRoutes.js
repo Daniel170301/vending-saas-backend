ec3b597d32ed4d8a446ad5516c91c626883ef705
Daniel <danieljaimes.2001@gmail.com>
2026-07-15 14:08:12 -0500

    Routes

--- routes/machineRoutes.js
+++ routes/machineRoutes.js
@@ -1,9 +1,15 @@
 // routes/machineRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getMachines } = require('../controllers/machineController');
 
-// Esta ruta será accesible en: /api/machines
+// 1. Importamos la nueva función updateMachine junto con getMachines
+const { getMachines, updateMachine } = require('../controllers/machineController');
+
+// Esta ruta será accesible en: GET /api/machines (Para leer la lista)
 router.get('/', getMachines);
+
+// 2. Agregamos la ruta PUT (Para actualizar una máquina específica)
+// Esta ruta será accesible en: PUT /api/machines/:id
+router.put('/:id', updateMachine);
 
 module.exports = router;
