70f5ef6f0c588c50f32ec1dbdcf452225962c3b2
Daniel <danieljaimes.2001@gmail.com>
2026-07-16 13:01:30 -0500

    Agregada ruta de historial.2

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 const salesController = require('../controllers/salesController');
 
-// Ruta: POST /api/ventas/confirm-dispense/:machine_id
+// Ruta 1: Confirmar el despacho físico y registrar la venta
 router.post('/confirm-dispense/:machine_id', salesController.confirmarDespacho);
+
+// Ruta 2: NUEVA - Obtener el historial de ventas para el Frontend
+router.get('/historial', salesController.obtenerHistorialVentas);
 
 module.exports = router;
