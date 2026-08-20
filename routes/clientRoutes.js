09480da7696526475a47c2f48e88ac7aebfd08de
Daniel <danieljaimes.2001@gmail.com>
2026-08-20 01:04:14 -0500

    cambios

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,16 +1,15 @@
 // routes/clientRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getUsers, createClient, getClients, updateClientStatus } = require('../controllers/clientController');
 
-// 1. Ruta para el selector de usuarios en el modal
-router.get('/users', getUsers); 
+// Importamos SOLO las 3 funciones que existen ahora en el controlador
+const { createClient, getClients, updateClientStatus } = require('../controllers/clientController');
 
-// 2. Rutas principales de la tabla y formulario
+// 1. Rutas principales de la tabla y formulario
 router.get('/', getClients); 
 router.post('/', createClient); 
 
-// 3. Ruta para el Switch (Habilitar/Deshabilitar)
+// 2. Ruta para el Switch (Habilitar/Deshabilitar)
 router.put('/:id/status', updateClientStatus); 
 
 module.exports = router;
