b5ad52efa278763cc40cf2c88769b5e911db6dfe
Daniel <danieljaimes.2001@gmail.com>
2026-08-20 01:20:05 -0500

    Correcion

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,9 +1,13 @@
 // routes/expenseRoutes.js
 const express = require('express');
 const router = express.Router();
-const { registerPurchase } = require('../controllers/expenseController');
+
+// AQUÍ ESTÁ EL CAMBIO: Agregamos getExpenses dentro de las llaves
+const { registerPurchase, getExpenses } = require('../controllers/expenseController');
+
 // Ruta para obtener la lista
 router.get('/', getExpenses);
+
 // Ruta para guardar una nueva compra de mercadería
 router.post('/purchase', registerPurchase);
 
