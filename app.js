1d19cd7c690f9e79a02ccba8664a032b25fbd8f8
Daniel <danieljaimes.2001@gmail.com>
2026-09-07 14:06:05 -0500

    comprobante

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js
-const express = require('express'); //[cite: 4]
-const cors = require('cors'); //[cite: 4]
-const authRoutes = require('./routes/authRoutes'); // Importamos la ruta
+const express = require('express'); 
+const cors = require('cors'); 
+const authRoutes = require('./routes/authRoutes'); 
 const inventoryRoutes = require('./routes/inventoryRoutes');
 const salesRoutes = require('./routes/salesRoutes');
 const reportRoutes = require('./routes/reportRoutes');
@@ -12,12 +12,17 @@ const warehouseRoutes = require('./routes/warehouseRoutes');
 const clientRoutes = require('./routes/clientRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 const proveedorRoutes = require('./routes/proveedorRoutes');
-const app = express(); //[cite: 4]
+const app = express(); 
 
 // Middlewares
-app.use(cors()); //[cite: 4]
-app.use(express.json()); //[cite: 4]
-app.use('/api/auth', authRoutes); // Ahora tu endpoint frontend será /api/auth/login
+app.use(cors()); 
+
+// LA CORRECCIÓN ESTÁ AQUÍ: Aumentamos el límite de JSON y URL-encoded a 10mb
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+// Rutas
+app.use('/api/auth', authRoutes); 
 app.use('/api/inventario', inventoryRoutes);
 app.use('/api/ventas', salesRoutes);
 app.use('/api/reportes', reportRoutes);
@@ -28,10 +33,10 @@ app.use('/api/productos-almacen', warehouseRoutes);
 app.use('/api/clients', clientRoutes);
 app.use('/api/gastos', expenseRoutes);
 app.use('/api/proveedores', proveedorRoutes);
-// Ruta base de prueba[cite: 4]
-app.get('/', (req, res) => {
-  res.send('Servidor SaaS de Máquinas Expendedoras 100% Operativo (Arquitectura Modular)'); //[cite: 4]
-});
 
+// Ruta base de prueba
+app.get('/', (req, res) => {
+  res.send('Servidor SaaS de Máquinas Expendedoras 100% Operativo (Arquitectura Modular)'); 
+});
 
 module.exports = app;
